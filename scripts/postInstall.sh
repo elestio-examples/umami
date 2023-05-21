0c70937857ef1d2ca22b9f2acc9a10236626d2b9
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-05-21 18:18:46 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,5 +3,30 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 30s;
+# sleep 30s;
 
+target=$(docker-compose port umami 3000)
+
+login=$(curl http://${target}/api/auth/login \
+  -H 'accept: application/json' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'authorization: Bearer undefined' \
+  -H 'cache-control: max-age=0' \
+  -H 'content-type: application/json' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36' \
+  -H 'x-umami-share-token: undefined' \
+  --data-raw '{"username":"admin","password":"umami"}' \
+  --compressed)
+
+token=$(echo $login | jq -r '.token')
+
+curl http://${target}/api/me/password \
+  -H 'accept: application/json' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'authorization: Bearer '${token}'' \
+  -H 'cache-control: max-age=0' \
+  -H 'content-type: application/json' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36' \
+  -H 'x-umami-share-token: undefined' \
+  --data-raw '{"currentPassword":"umami","newPassword":"'${ADMIN_PASSWORD}'","confirmPassword":"'${ADMIN_PASSWORD}'"}' \
+  --compressed
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,6 +1,4 @@
 set env vars
 set -o allexport; source .env; set +o allexport;
 
-newPassword=$(mkpasswd -m bcrypt "${ADMIN_PASSWORD}")
-
-sed -i "s~PASSWORD~${newPassword}~g" ./sql/schema.postgresql.sql
+apt install jq -y
