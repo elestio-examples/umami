bae70031c3a22f14f418a272ec5003d829f448b8
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-21 13:58:39 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,1 +1,3 @@
-docker buildx build --build-arg DATABASE_TYPE=postgresql . --output type=docker,name=elestio4test/umami:latest | docker load
+sed -i "s~ENV DATABASE_TYPE \$DATABASE_TYPE~ENV DATABASE_TYPE postgresql~g" ./Dockerfile
+
+docker buildx build . --output type=docker,name=elestio4test/umami:latest | docker load
